4099963fa6b629a5b2da37e35017f5257fc84fa0
Nikolas Burk <nikolas.burk@gmail.com>
2018-05-03 12:14:04 +0200

    ex3

---
+++ ex3/gateway/src/index.js
@@ -0,0 +1,112 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { addFragmentToInfo } = require('graphql-binding')
+const generateName = require('sillyname')
+
+const RemoteBinding = require('./RemoteBinding')
+
+const userServiceBinding = new RemoteBinding()
+const postServiceBinding = new RemoteBinding()
+
+const typeDefs = `
+# import Post from '../schemas/post-service.graphql'
+# import User from '../schemas/user-service.graphql'
+
+type Query {
+  posts(searchString: String): [Post!]!
+  user(id: ID!): User
+}
+
+type Mutation {
+  createDraft(title: String!, content: String!): Post
+  publish(id: ID!): Post
+  deletePost(id: ID!): Post
+  login(): User!
+  changeName(id: ID!, newName: String!): User
+}
+`
+
+const resolvers = {
+  Query: {
+    posts: (_, args, context, info) => {
+      const searchString = args.searchString ? args.searchString : ''
+      const ensureTitleAndContentFragment = `
+        fragment EnsureTitleAndContentFragment on Post {
+          title
+          content
+        }
+      `
+      return context.postService.query
+        .posts({}, addFragmentToInfo(info, ensureTitleAndContentFragment))
+        .filter(
+          post =>
+            post.title.includes(searchString) ||
+            post.content.includes(searchString),
+        )
+    },
+    user: (_, args, context, info) => {
+      return context.postService.mutation.user(
+        {
+          id: args.id,
+        },
+        info,
+      )
+    },
+  },
+  Mutation: {
+    createDraft: (_, args, context, info) => {
+      return context.postService.mutation.createPost(
+        {
+          title: args.title,
+          content: args.content,
+          published: false,
+        },
+        info,
+      )
+    },
+    publish: (_, args, context, info) => {
+      return context.postService.mutation.updatePost(
+        {
+          id: args.id,
+          published: true,
+        },
+        info,
+      )
+    },
+    deletePost: (_, args, context, info) => {
+      return context.postService.deletePost(
+        {
+          id: args.id,
+        },
+        info,
+      )
+    },
+    login: (_, args, context, info) => {
+      const sillyName = generateName()
+      return context.userService.createUser(
+        {
+          name: sillyname,
+        },
+        info,
+      )
+    },
+    changeName: (_, args, context, info) => {
+      return context.userServiceBinding.updateUser(
+        {
+          id: args.id,
+          name: newName,
+        },
+        info,
+      )
+    },
+  },
+}
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+  context: req => ({
+    userService: userServiceBinding,
+    postService: postServiceBinding,
+  }),
+})
+server.start(() => console.log(`Server is running on http://localhost:4000`))
