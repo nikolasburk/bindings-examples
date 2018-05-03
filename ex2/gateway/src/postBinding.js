da1b5164a7201b3f9fae33d75cdca0cd252cf2be
Johannes Schickling <schickling.j@gmail.com>
2018-05-03 12:35:41 +0200

    ex2 WIP

--- ex2/gateway/src/postBinding.js
+++ ex2/gateway/src/postBinding.js
@@ -1,6 +1,14 @@
+const fs = require('fs')
+const path = require('path')
 const { makeBinding } = require('graphql-binding')
-// const = require('graphql-')
-const { schema } = require('../../post-service/src/schema')
-const 
+const { HttpLink } = require('apollo-link-http')
+const fetch = require('node-fetch')
+const { makeRemoteExecutableSchema } = require('graphql-tools')
 
-module.exports = Binding
+const link = new HttpLink({ uri: 'http://localhost:4001', fetch })
+const schema = makeRemoteExecutableSchema({
+  schema: fs.readFileSync(path.join(__dirname, '../schemas/post-service.graphql')),
+  link,
+})
+
+module.exports = makeBinding(schema)
--- ex2/post-service/src/server.js
+++ ex2/post-service/src/server.js
@@ -2,4 +2,6 @@ const { GraphQLServer } = require('graphql-yoga')
 const schema = require('./schema')
 
 const server = new GraphQLServer({ schema })
-server.start(() => console.log(`Server is running at http://localhost:4000`))
+server.start({ port: 4001 }, () =>
+  console.log(`Server is running at http://localhost:4001`),
+)
