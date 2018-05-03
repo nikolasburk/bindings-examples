577a4256838d3ebfd2c0eb6088b45822485fffaf
Johannes Schickling <schickling.j@gmail.com>
2018-05-03 12:39:24 +0200

    ex2

--- ex2/gateway/src/postBinding.js
+++ ex2/gateway/src/postBinding.js
@@ -6,9 +6,11 @@ const fetch = require('node-fetch')
 const { makeRemoteExecutableSchema } = require('graphql-tools')
 
 const link = new HttpLink({ uri: 'http://localhost:4001', fetch })
-const schema = makeRemoteExecutableSchema({
-  schema: fs.readFileSync(path.join(__dirname, '../schemas/post-service.graphql')),
-  link,
-})
+const typeDefs = fs.readFileSync(
+  path.join(__dirname, '../schemas/post-service.graphql'),
+  'utf-8',
+)
+
+const schema = makeRemoteExecutableSchema({ link, schema: typeDefs })
 
 module.exports = makeBinding(schema)
